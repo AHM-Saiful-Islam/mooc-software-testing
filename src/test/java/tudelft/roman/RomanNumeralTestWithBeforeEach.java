5f246ba8ca5f05d3adeeebd2512b9c18b4376a58
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2025-08-03 15:08:06 +0200

    new test case & Failed for invalid input RomanNumeralTestWithBeforeEach.java

--- src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
+++ src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
@@ -36,4 +36,39 @@ public class RomanNumeralTestWithBeforeEach {
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
+
+    // new test case
+    @Test
+    public void numberWithComplexDigits() {
+        int result = roman.convert("XCIX");
+        Assertions.assertEquals(99, result);
+    }
+    
+    // new test case
+    @Test
+    public void numberWithWrongDigits() {
+        int result = roman.convert("IX");
+        Assertions.assertNotEquals(11, result);
+    }
+
+    // VV is invalid input. but it's giving correct result
+    @Test
+    public void numberWithInvalidDigits() {
+        int result = roman.convert("VV");
+        Assertions.assertNotEquals(10, result);
+    }
+
+    // IC is invalid input. but it's giving correct result
+    @Test
+    public void numberWithInvalidDigits2() {
+        int result = roman.convert("IC");
+        Assertions.assertNotEquals(99, result);
+    }
+
+    // IL is invalid input. but it's giving correct result
+    @Test
+    public void numberWithInvalidDigits3() {
+        int result = roman.convert("IL");
+        Assertions.assertNotEquals(49, result);
+    }
 }
