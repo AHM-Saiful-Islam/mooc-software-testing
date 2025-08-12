e4c001531e1efa4ab4a41abd6b676fa3fbd99c1b
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2025-08-13 00:46:08 +0200

    Bug Detected and solved GHappy.java

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -6,7 +6,9 @@ public class GHappy {
         assert str!=null;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
+                // Bug Detected
+                // if (i >= 0 && str.charAt(i-1) == 'g') { continue; } // i>=0 and i-1 indicate index -1, create error
+                if (i > 0 && str.charAt(i-1) == 'g') { continue; } 
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
